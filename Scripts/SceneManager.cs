63cb3528edd376ced343434ab45dccdab7305f95
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-11-12 18:36:51 -0700

    Add files via upload

---
+++ Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+    }
+
+    private static void LoadScene(string v, LoadSceneMode additive)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Scripts/SelectionManager.cs
@@ -0,0 +1,39 @@
+﻿﻿using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    private IRayProvider _rayProvider;
+    private ISelector _selector;
+    private ISelectionResponse _selectionResponse;
+
+    private Transform _currentSelection;
+
+    private void Awake()
+    {
+        _selectionResponse = GetComponent<ISelectionResponse>();
+        _rayProvider = GetComponent<IRayProvider>();
+        _selector = GetComponent<ISelector>();
+    }
+
+    private void Update()
+    {
+        if (_currentSelection != null)
+        {
+            _selectionResponse.OnDeselect(_currentSelection);
+        }
+        var ray = _rayProvider.CreateRay();
+
+        _selector.Check(ray);
+
+        _currentSelection = _selector.GetSelection() ;
+
+        if (_currentSelection != null)
+        {
+            _selectionResponse.OnSelect(_currentSelection);
+        }
+    }
+}
+
+
+
+
